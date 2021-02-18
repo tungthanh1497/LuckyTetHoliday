58e057d2971215acd6a514fb93ae22c0d2e1f340
tungtt <tungthanh1497@gmail.com>
2021-02-18 16:22:21 +0700

    add comments

--- main.go
+++ main.go
@@ -23,29 +23,37 @@ func startGame(listPlayers *[]Player) {
 	existedCards := make(map[string]bool)
 	for position := 0; position < len(*listPlayers); position++ {
 		for i := 0; i < 3; i++ {
+			// randomize 3 cards
 			card := getRandomCard(&existedCards)
 			existedCards[card] = true
 			(*listPlayers)[position].listCard[i] = card
 		}
+		// check sum and type won for each person
 		calSum(&((*listPlayers)[position]))
 	}
 	sort.SliceStable(*listPlayers, func(x, y int) bool {
 		xPlayer := (*listPlayers)[x]
 		yPlayer := (*listPlayers)[y]
 		if xPlayer.typeWon != yPlayer.typeWon {
+			// sort type won: triple > sum 10 > normal
 			return xPlayer.typeWon > yPlayer.typeWon
 		} else if 3 == xPlayer.typeWon {
+			// case triple: sort by value
 			xCheckValue := getCheckValue(xPlayer.listCard[0])
 			yCheckValue := getCheckValue(yPlayer.listCard[0])
 			return xCheckValue > yCheckValue
 		} else if xPlayer.sum != yPlayer.sum {
+			// case sum 10 or normal: sort by sum
 			return xPlayer.sum > yPlayer.sum
 		} else {
+			// case sum equal
 			xMaxSuit := getMaxSuit(xPlayer.listCard)
 			yMaxSuit := getMaxSuit(yPlayer.listCard)
 			if xMaxSuit != yMaxSuit {
+				// sort by largest suit
 				return xMaxSuit > yMaxSuit
 			} else {
+				// sort by largest value of largest suit
 				xMaxValue := getMaxValue(xPlayer.listCard, xMaxSuit)
 				yMaxValue := getMaxValue(yPlayer.listCard, yMaxSuit)
 				return xMaxValue > yMaxValue
@@ -54,6 +62,9 @@ func startGame(listPlayers *[]Player) {
 	})
 }
 
+/**
+get max value in list card (by max suit)
+*/
 func getMaxValue(listCard [3]string, maxSuit int) int {
 	maxValue := 0
 	for i := 0; i < len(listCard); i++ {
@@ -70,6 +81,9 @@ func getMaxValue(listCard [3]string, maxSuit int) int {
 	return maxValue
 }
 
+/**
+get max suit in list card
+*/
 func getMaxSuit(listCard [3]string) int {
 	maxSuit := 0
 	for i := 0; i < len(listCard); i++ {
@@ -82,6 +96,9 @@ func getMaxSuit(listCard [3]string) int {
 	return maxSuit
 }
 
+/**
+case triple, get check value (any value in list cards)
+*/
 func getCheckValue(card string) int {
 	if 1 == card[0] {
 		return 10
@@ -99,8 +116,10 @@ func calSum(player *Player) {
 		value, _ := strconv.Atoi((*player).listCard[i][0:1])
 		sum += value
 		if 0 == checkingValue {
+			// first card
 			checkingValue = value
 		} else if checkingValue != value {
+			// if 2 cards have different value -> can not be triple
 			isTriple = false
 		}
 	}
@@ -117,6 +136,15 @@ func calSum(player *Player) {
 	}
 }
 
+/**
+randomize a card for user contain
+* value: from 1-9
+* suit: from 0 to 3
+   + 0: spade
+   + 1: club
+   + 2: heart
+   + 3: diamond
+*/
 func getRandomCard(existedCards *map[string]bool) string {
 	for {
 		value := rand.Intn(9) + 1
@@ -130,6 +158,9 @@ func getRandomCard(existedCards *map[string]bool) string {
 	}
 }
 
+/**
+init players dex
+*/
 func initPlayers() []Player {
 	numPlayers := inputNumPlayers()
 	var listPlayers []Player
@@ -140,12 +171,16 @@ func initPlayers() []Player {
 	return listPlayers
 }
 
+/**
+get name of user by position
+name can not be empty
+*/
 func inputName(position int) string {
 	var name string
 	fmt.Printf("Enter name of player %d: ", position+1)
 	for {
 		_, err := fmt.Scanf("%s", &name)
-		if err != nil {
+		if err != nil || len(name) == 0 {
 			fmt.Printf("%s. Try again: ", err)
 		} else {
 			return name
@@ -153,6 +188,10 @@ func inputName(position int) string {
 	}
 }
 
+/**
+get number of players from input
+must be a number larger than 0 and smaller than 12
+*/
 func inputNumPlayers() int {
 	var numPlayer int
 	fmt.Print("Enter number of players: ")
