c162130ea0a1b0d3e99fa425cc6a561825117506
tungtt <tungthanh1497@gmail.com>
2021-02-18 12:02:23 +0700

    input num of players and names

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var numPlayers int
+	var listPlayers []Player
+
+	numPlayers, listPlayers = initPlayers()
+
+	fmt.Println(numPlayers)
+	fmt.Println(listPlayers)
+}
+
+func initPlayers() (int, []Player) {
+	numPlayers := inputNumPlayers()
+	var listPlayers []Player
+	for i := 0; i < numPlayers; i++ {
+		name := inputName(i)
+		listPlayers = append(listPlayers, Player{name: name})
+	}
+	return numPlayers, listPlayers
+}
+
+func inputName(position int) string {
+	var name string
+	fmt.Printf("Enter name of player %d: ", position+1)
+	for {
+		_, err := fmt.Scanf("%s", &name)
+		if err != nil {
+			fmt.Printf("%s. Try again: ", err)
+		} else {
+			return name
+		}
+	}
+}
+
+func inputNumPlayers() int {
+	var numPlayer int
+	fmt.Print("Enter number of players: ")
+	for {
+		_, err := fmt.Scanf("%d", &numPlayer)
+		if err != nil {
+			fmt.Printf("%s. Try again: ", err)
+		} else {
+			return numPlayer
+		}
+	}
+
+}
+
+type Player struct {
+	name     string
+	listCard []string
+	sum      int
 }
